4f92f33afa9157e7c59a619202f3fba6ba8e5a12
macnaer <dev@A33PC15>
2023-07-07 15:56:53 +0300

    Add project files.

---
+++ Walter.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walter.Infrastructure.Context
+{
+    internal class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() : base() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
---
+++ Walter.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Walter.Infrastructure.Context;
+
+namespace Walter.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+    }
+}
