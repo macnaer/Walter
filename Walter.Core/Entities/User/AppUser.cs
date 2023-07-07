53181584051499b0625bfb4230d0be9f21940c36
macnaer <dev@A33PC15>
2023-07-07 16:25:15 +0300

    Extends Identity user

---
+++ Walter.Core/Entities/User/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walter.Core.Entities.User
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
--- Walter.Infrastructure/Context/AppDbContext.cs
+++ Walter.Infrastructure/Context/AppDbContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Walter.Core.Entities.User;
 
 namespace Walter.Infrastructure.Context
 {
@@ -13,5 +14,6 @@ namespace Walter.Infrastructure.Context
         public AppDbContext() : base() { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<AppUser> AppUsers { get; set; }
     }
 }
