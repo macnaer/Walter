218714a47e287f48400eb0539e48c08986729f10
trofimchuk <trofimchuk@itstep.academy>
2023-07-10 16:27:08 +0300

    Start with dashboard layout

---
+++ Walter.Web/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Walter.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
