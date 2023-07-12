9fe4b1b3ef8b5082ad956a14fd008f82d9b024d1
trofimchuk <trofimchuk@itstep.academy>
2023-07-12 16:05:21 +0300

    Before validation

---
+++ Walter.Core/DTO's/User/LoginUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walter.Core.DTO_s.User
+{
+    public class LoginUserDto
+    {
+        public string Email { get; set;} = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; }
+    }
+}
--- Walter.Web/Controllers/DashboardController.cs
+++ Walter.Web/Controllers/DashboardController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Walter.Core.DTO_s.User;
 
 namespace Walter.Web.Controllers
 {
+    [Authorize]
     public class DashboardController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        [AllowAnonymous] // Method GET
+        public async Task<IActionResult> SignIn()
+        {
+            return View();
+        }
+
+
+        [AllowAnonymous] // Method POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignIn(LoginUserDto model)
+        {
+            return View();
+        }
+
     }
 }
