bed64240f6043806fa6901db350dc3c4e53ec746
trofimchuk <trofimchuk@itstep.academy>
2023-08-30 16:20:56 +0300

    Create post form

---
+++ Walter.Web/Controllers/PostController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Walter.Core.DTO_s.Category;
+using Walter.Core.Interfaces;
+
+namespace Walter.Web.Controllers
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public PostController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Create()
+        {
+            await LoadCategories();
+            return View();
+        }
+
+        private async Task LoadCategories()
+        {
+            ViewBag.CategoryList = new SelectList(
+                await _categoryService.GettAll(),
+                nameof(CategoryDto.Id),
+                nameof(CategoryDto.Name)
+                );
+            ;
+        }
+    }
+}
